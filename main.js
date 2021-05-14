46f3b8ca87638761dbe72e39406e0d430e5c49d0
jaspermarsman <jaspermarsman@gmail.com>
2021-05-14 14:54:46 +0200

    error message will be deleted on new search

--- main.js
+++ main.js
@@ -8,38 +8,47 @@
 let userInput = "";
 
 async function getCountryInformation() {
-    const response = await axios.get(`https://restcountries.eu/rest/v2/name/${userInput}`);
-    console.log(response.data);
-    const countryName = response.data[0].name;
-    const subareaName = response.data[0].subregion;
-    const capitalName = response.data[0].capital;
-    const countryFlag = response.data[0].flag
-    const currencies = (response.data[0].currencies);
+    try {
+        const response = await axios.get(`https://restcountries.eu/rest/v2/name/${userInput}`);
+        console.log(response.data);
+        const countryName = response.data[0].name;
+        const subareaName = response.data[0].subregion;
+        const capitalName = response.data[0].capital;
+        const countryFlag = response.data[0].flag
+        const currencies = (response.data[0].currencies);
 
-    let countryCurrencies = "";
-    currencyString(currencies);
+        let countryCurrencies = "";
+        currencyString(currencies);
 
-    function currencyString(currencies) {
+        function currencyString(currencies) {
 
-        if (currencies.length < 2) {
-            countryCurrencies = `and you can pay with ${response.data[0].currencies[0].name}'s`;
-        } else {
-            countryCurrencies = `and you can pay with ${response.data[0].currencies[0].name}'s and ${response.data[0].currencies[1].name}'s`;
-        }
-        return countryCurrencies;
-    };
+            if (currencies.length < 2) {
+                countryCurrencies = `and you can pay with ${response.data[0].currencies[0].name}'s`;
+            } else {
+                countryCurrencies = `and you can pay with ${response.data[0].currencies[0].name}'s and ${response.data[0].currencies[1].name}'s`;
+            }
+            return countryCurrencies;
+        };
 
-    const countryFlagElement = document.getElementById("flag-image");
-    countryFlagElement.setAttribute('src', countryFlag);
+        const countryFlagElement = document.getElementById("flag-image");
+        countryFlagElement.setAttribute('src', countryFlag);
 
-    const countryNameElement = document.getElementById("country-name");
-    countryNameElement.textContent = `${countryName}`;
+        const countryNameElement = document.getElementById("country-name");
+        countryNameElement.textContent = `${countryName}`;
 
-    const countryInfoElement = document.getElementById("country-info");
-    countryInfoElement.textContent = `${countryName} is situated in ${subareaName}. 
+        const countryInfoElement = document.getElementById("country-info");
+        countryInfoElement.textContent = `${countryName} is situated in ${subareaName}. 
     The capital is ${capitalName} ${countryCurrencies}.`;
 
-    searchElement.value = "";
+        searchElement.value = "";
+        const errorMessageElement = document.getElementById("error-field");
+        errorMessageElement.textContent = "";
+    }
+    catch (e) {
+        console.log("error");
+        const errorMessageElement = document.getElementById("error-field");
+        errorMessageElement.textContent = "This is an invalid search input. Please, search for an existing country.";
+    }
 };
 
 
